73b94d1d1adff2325b6fef80cc5f558291fa46b7
Neil Vaytet <neil.vaytet@esss.se>
2018-10-15 13:58:20 +0200

    tab reformat

--- keyboard-window-focus.sh
+++ keyboard-window-focus.sh
@@ -14,32 +14,32 @@ mapfile -t windows < <( echo "${window_list}" );
 # Make an array with the window ids and window positions
 n=0;
 for ((i=0;i<${#windows[@]};i++)); do
-	thisLine=( $(echo "${windows[i]}") );
-	# Check if window is minimized or not
-	winState=$(xprop -id ${thisLine[0]} | grep "window state" | cut -d ':' -f2);
-	if [ $winState == "Normal" ] ; then
-		winId[n]=${thisLine[0]};
-		winX[n]=${thisLine[2]};
-		winY[n]=${thisLine[3]};
-		n=$(($n + 1));
-	fi
+    thisLine=( $(echo "${windows[i]}") );
+    # Check if window is minimized or not
+    winState=$(xprop -id ${thisLine[0]} | grep "window state" | cut -d ':' -f2);
+    if [ $winState == "Normal" ] ; then
+        winId[n]=${thisLine[0]};
+        winX[n]=${thisLine[2]};
+        winY[n]=${thisLine[3]};
+        n=$(($n + 1));
+    fi
 done
 
 # Sort windows left to right with bubble sort
 for ((i=0;i<$n;i++)); do
-	for ((j=0;j<$(($n - 1));j++)); do
-		if [ ${winX[j]} -gt ${winX[j+1]} ] ; then
-			tempId=${winId[j+1]};
-			tempX=${winX[j+1]};
-			tempY=${winY[j+1]};
-			winId[j+1]=${winId[j]};
-			winX[j+1]=${winX[j]};
-			winY[j+1]=${winY[j]};
-			winId[j]=${tempId};
-			winX[j]=${tempX};
-			winY[j]=${tempY};
-		fi
-	done
+    for ((j=0;j<$(($n - 1));j++)); do
+        if [ ${winX[j]} -gt ${winX[j+1]} ] ; then
+            tempId=${winId[j+1]};
+            tempX=${winX[j+1]};
+            tempY=${winY[j+1]};
+            winId[j+1]=${winId[j]};
+            winX[j+1]=${winX[j]};
+            winY[j+1]=${winY[j]};
+            winId[j]=${tempId};
+            winX[j]=${tempX};
+            winY[j]=${tempY};
+        fi
+    done
 done
 
 # Find currently focused window
@@ -48,25 +48,25 @@ focusedWindow=${focusedWindowString[1]};
 
 # Add extra zero to ensure compatibility between wmctrl and xprop
 if [ ${#focusedWindow} -lt ${#winId[0]} ]; then
-	focusedWindow=$(echo "${focusedWindow}" | sed 's/0x/0x0/g');
+    focusedWindow=$(echo "${focusedWindow}" | sed 's/0x/0x0/g');
 fi
 
 # Now loop through list of windows, find currenty focused window
 # and switch to the window on the left or right depending on input
 for ((i=0;i<$n;i++)); do
-	if [ "${winId[i]}" == "${focusedWindow}" ] ; then
-		if [ "${direction}" == "left" ] ; then
-			if [ $i -gt 0 ] ; then
-				wmctrl -i -a "${winId[i-1]}";
-      else
-        wmctrl -i -a "${winId[n-1]}";
-			fi
-		elif [ "${direction}" == "right" ] ; then
-			if [ $i -lt $(($n - 1)) ] ; then
-				wmctrl -i -a "${winId[i+1]}";
-      else
-        wmctrl -i -a "${winId[0]}";
-			fi
-		fi
-	fi
+    if [ "${winId[i]}" == "${focusedWindow}" ] ; then
+        if [ "${direction}" == "left" ] ; then
+            if [ $i -gt 0 ] ; then
+                wmctrl -i -a "${winId[i-1]}";
+            else
+                wmctrl -i -a "${winId[n-1]}";
+            fi
+        elif [ "${direction}" == "right" ] ; then
+            if [ $i -lt $(($n - 1)) ] ; then
+                wmctrl -i -a "${winId[i+1]}";
+            else
+                wmctrl -i -a "${winId[0]}";
+            fi
+        fi
+    fi
 done
